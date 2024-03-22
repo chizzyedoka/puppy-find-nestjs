2fbe701cedde67722e4be5e81bba74bb83aa39f6
CHISOM EDOKA <edokachisom48@gmail.com>
2024-03-23 00:53:16 +0100

    added user services

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,34 +7,46 @@ import {
   Patch,
   Post,
   Put,
+  Query,
 } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
-  // constructor(private userService: UserService) {}
-  @Get() // GET /users
-  findAllUsers(): [] {
-    return [];
+  constructor(private readonly userService: UsersService) {}
+  @Get() // GET /users?type=customer
+  findAllUsers(@Query('type') type?: 'buyer' | 'seller') {
+    return this.userService.findAllUsers(type);
   }
 
   // GET /users/:id
   @Get(':id')
   findOneUser(@Param('id') id: string) {
-    return { id };
+    return this.userService.findOneUser(parseInt(id));
   }
 
   @Post() // POST /users
-  create(@Body() user: {}) {
-    return user;
+  createUser(
+    @Body() user: { name: string; email: string; userType: 'buyer' | 'seller' },
+  ) {
+    return this.userService.createUser(user);
   }
 
   @Patch(':id') // PATCH /users/:id
-  findOneAndUpdate(@Param('id') id: string, @Body() userUpdate: {}) {
-    return { id, ...userUpdate };
+  update(
+    @Param('id') id: string,
+    @Body()
+    userUpdate: {
+      name?: string;
+      email?: string;
+      userType?: 'buyer' | 'seller';
+    },
+  ) {
+    return this.userService.update(+id, userUpdate);
   }
 
   @Delete(':id') // DELETE /users/:id
-  findAndDelete(@Param('id') id: string) {
-    return { id };
+  delete(@Param('id') id: string) {
+    return this.userService.delete(+id);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,78 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users = [
+    {
+      id: 1,
+      name: 'chizzy',
+      email: 'chizzy@gmail.com',
+      userType: 'seller',
+    },
+    {
+      id: 2,
+      name: 'tola',
+      email: 'tola@gmail.com',
+      userType: 'buyer',
+    },
+    {
+      id: 3,
+      name: 'obinna',
+      email: 'obinna@gmail.com',
+      userType: 'seller',
+    },
+    {
+      id: 4,
+      name: 'tolu',
+      email: 'tolu@gmail.com',
+      userType: 'buyer',
+    },
+  ];
+
+  findAllUsers(type?: 'buyer' | 'seller') {
+    if (type) {
+      return this.users.filter((user) => user.userType === type);
+    }
+    return this.users;
+  }
+
+  findOneUser(id: number) {
+    const user = this.users.find((user) => user.id === id);
+    if (!user) return 'Not Found';
+    return user;
+  }
+
+  createUser(user: {
+    name: string;
+    email: string;
+    userType: 'buyer' | 'seller';
+  }) {
+    const usersByHighestId = [...this.users].sort((a, b) => b.id - a.id);
+    const newUser = { id: usersByHighestId[0].id + 1, ...user };
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  update(
+    id: number,
+    updatedUser: {
+      name?: string;
+      email?: string;
+      userType?: 'buyer' | 'seller';
+    },
+  ) {
+    this.users = this.users.map((user) => {
+      if (user.id === id) {
+        return { ...user, ...updatedUser };
+      }
+      return user;
+    });
+    return this.findOneUser(id);
+  }
+
+  delete(id: number) {
+    const removedUser = this.findOneUser(id);
+    this.users = this.users.filter((user) => user.id !== id);
+    return removedUser;
+  }
+}
